0729d5b5988cd4c1e2624c54869f20aea63fa369
jiel <nikhilpagadala2006@gmail.com>
2025-09-30 23:57:26 +0530

    put request

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import {
   addingStock,
-  gettingStocks
+  gettingStocks,
+  updateStock,
+  deleteStock
 } from '../controllers/stockControllers.js';
 const router = express.Router();
 router.post('/addStock',  addingStock);
 router.get('/getStocks',  gettingStocks);
+router.put('/updateStock/:symbol', updateStock);
+router.delete('/deleteStock/', deleteStock);
 export default router;
