d765b5b1b6ac3942b4b53095ba81487544b78e4c
jiel <nikhilpagadala2006@gmail.com>
2025-11-11 00:02:29 +0530

    jjj

--- backend/db.js
+++ backend/db.js
@@ -4,15 +4,16 @@ import dotenv from "dotenv";
 // Load env vars
 dotenv.config();
 
-
-const connectDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}`);
-        console.log(`MongoDB is connected! DB Host: ${connectionInstance.connection.host}`);                                                              
-    } catch (error) {
-        console.log("ERROR: ", error);
-        process.exit(1);
-    }
+const connectDB = () => {
+    mongoose
+        .connect(process.env.MONGODB_URL)
+        .then((connectionInstance) => {
+            console.log(`MongoDB is connected! DB Host: ${connectionInstance.connection.host}`);
+        })
+        .catch((error) => {
+            console.log("ERROR:", error);
+            process.exit(1);
+        });
 };
 
 export default connectDB;
